216ba4b0ec4956e400db0cbe6000b625c9b13790
Genvara <nadjetissam@gmail.com>
2020-07-15 12:51:38 +0100

    fixing comments: episode 2

--- network-batch.bat
+++ network-batch.bat
@@ -1,8 +1,8 @@
 :: PLEASE READ
-:: this is network-batch, a script that might fix network :: stuff, however i do not promise that all of your problems :: will be fixed.
-:: you have to configure this script before running :: it, follow the documentation written in the script in :: order to setup.
-:: this should be used by someone that knows what is :: happening in this script, if you don't know what the hell :: is going on, don't execute this script, or call-up a :: friend that knows this stuff and let them set it up for :: you.
-:: i am not responsible for any damages caused by this :: script, if anything breaks, it's not my fault, you have :: been warned.
+:: this is network-batch, a script that might fix network stuff, however i do not promise that all of your problems will be fixed.
+:: you have to configure this script before running it, follow the documentation written in the script in order to setup.
+:: this should be used by someone that knows what is happening in this script, if you don't know what the hell is going on, don't execute this script, or call-up a friend that knows this stuff and let them set it up for you.
+:: i am not responsible for any damages caused by this script, if anything breaks, it's not my fault, you have been warned.
 
 :: disable and re-enable adapters
 :: change 'value' with the name of your network adapter that you use to connect to the internet (Ex: Ethernet, Wi-Fi, Wi=Fi 2, etc...)
